b9dfa03688520d0792b17356380cd55308afd6f2
seonhop <78877741+seonhop@users.noreply.github.com>
2022-06-17 17:34:40 -0700

    more photos + weather background

--- js/background.js
+++ js/background.js
@@ -1,4 +1,14 @@
-const images = ["01.jpeg", "02.jpeg", "03.jpeg"];
+const images = [
+    "01.jpeg",
+    "02.jpeg",
+    "03.jpeg",
+    "04.jpeg",
+    "05.jpeg",
+    "06.jpeg",
+    "07.jpeg",
+    "08.jpeg",
+    "09.jpeg",
+];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
